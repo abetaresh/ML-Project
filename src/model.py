7521d5cbb769ff88f733ca89f97da8018f1d7793
Yann Hoffmann <hyann98@gmail.com>
2020-12-12 10:42:39 +0100

    .

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from skimage.io import imread, imshow
 from start import y_train, y_test
+import plotly.express as px
 
 # %%
 # Idea 1: Train separate models on the groups
@@ -15,7 +16,7 @@ label_group = [
 
 
 # %%
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 def luminosity(df):
     df["f_lum"] = list(map(lambda x: np.mean(imread(x)), "../data/train/" + df.index))
     return df
@@ -23,14 +24,13 @@ def luminosity(df):
 y_train = luminosity(y_train)
 
 # %%
-import plotly.express as px
 px.scatter_3d(y_train, x="day", y="night", z="f_lum")
 
 # %%
 
 X = y_train[["f_lum"]]
 y = y_train[["day", "night"]]
-reg = LinearRegression().fit(X, y)
+reg = LogisticRegression().fit(X, y)
 reg.score(X, y)
 
 # %%
